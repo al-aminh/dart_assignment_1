801f1a30d0b5e14796436795ccaf78864551c5a4
al-aminh <alaminhossain123245@gmail.com>
2025-06-21 23:59:54 +0600

    ans 7

---
+++ question7.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  int? x = int.parse(stdin.readLineSync()!);
+  int? y = int.parse(stdin.readLineSync()!);
+  
+  print("Quotient: ${x ~/ y}");
+  print("Reminder: ${x % y}");
+
+
+}
